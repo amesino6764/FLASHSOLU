193b01ff5d125a4e2ed2fc340ef38dcc9c25ddee
amesino6764 <85662958+amesino6764@users.noreply.github.com>
2021-09-18 17:16:24 -0500

    Add files via upload

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'FLASHSOLU',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
